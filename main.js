29a3cfdd40828387e58d308b0d6a9af1c7186068
freegut <147081821+freegut@users.noreply.github.com>
2024-06-17 12:26:40 +0400

    update
    new

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
-const axios = require('axios');
-
 // Функция для запроса фактов о кошках
 async function getCatFact() {
     try {
         const response = await axios.get('https://catfact.ninja/fact');
-        document.getElementById('output').innerHTML += `<p>Cat Fact: ${response.data.fact}</p>`;
+        document.getElementById('catFactText').textContent = response.data.fact;
     } catch (error) {
         console.error('Error fetching cat fact:', error);
     }
@@ -24,9 +22,34 @@ async function getNationality(name) {
 async function getRandomDogImage() {
     try {
         const response = await axios.get('https://dog.ceo/api/breeds/image/random');
-        document.getElementById('output').innerHTML += `<p>Random Dog Image: <img src="${response.data.message}" alt="Random Dog"></p>`;
+        document.getElementById('dogImageElement').src = response.data.message;
     } catch (error) {
         console.error('Error fetching dog image:', error);
+    }
+}
+
+// Функция для получения информации о игре из RAWG API
+async function getGameInfo() {
+    const gameName = document.getElementById('gameInput').value;
+    if (!gameName) {
+        alert('Пожалуйста, введите название игры.');
+        return;
+    }
+    try {
+        const response = await axios.get(`https://api.rawg.io/api/games?key=4e56a5906c7d4a2eb536b12a154ac416&search=${gameName}`);
+        document.getElementById('gameInfoText').textContent = JSON.stringify(response.data, null, 2);
+    } catch (error) {
+        console.error('Error fetching game info:', error);
+    }
+}
+
+// Функция для получения новостной статьи из The Guardian API
+async function getNewsArticle() {
+    try {
+        const response = await axios.get('https://content.guardianapis.com/search?api-key=a26e6b95-1c73-44f7-89b2-0bbafe86a0e6');
+        document.getElementById('newsArticleText').textContent = JSON.stringify(response.data.response.results[0].webTitle, null, 2);
+    } catch (error) {
+        console.error('Error fetching news article:', error);
     }
 }
 
